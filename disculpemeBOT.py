b5a9b71cb05a3a7e2ded69da360fa01acbef8093
Paco Brevers <francisco.brevers@tecnilogica.com>
2016-11-28 13:20:03 +0100

    Now favorites will not generate a empty reply.

--- disculpemeBOT.py
+++ disculpemeBOT.py
@@ -1,3 +1,12 @@
+"""
+ ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <etiorum@gmail.com> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Paco Brevers
+ * ----------------------------------------------------------------------------
+"""
+
 import configparser
 import json
 import os
@@ -34,13 +43,14 @@ class ReplyToTweet(tweepy.StreamListener):
         tweet = json.loads(data.strip())
 
         retweeted = tweet.get('retweeted', False)
+        event = tweet.get('event', False)
 
         if 'screen_name' not in data or tweet.get('user', {}).get('screen_name', '') == account_user_id:
             from_self = True
         else:
             from_self = False
 
-        if retweeted is not None and not retweeted and not from_self:
+        if retweeted is not None and not retweeted and event is not None and not event and not from_self:
             tweet_id = tweet.get('id_str')
             screen_name = tweet.get('user', {}).get('screen_name', '')
 
